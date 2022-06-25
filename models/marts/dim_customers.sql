df47a53014c442108cba66f69f1db6a3dce5233d
Natana Costa <natanapedrosoeng@gmail.com>
2022-06-25 04:32:12 +0000

    dev

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -5,30 +5,33 @@ with
             , region_id
             , company_name
             , city
+            , region
             , country
         from {{ref('stg_customers')}}
     )
     , selected_seed_regions as (
         select
             id
+            , city
             , region
             , division
         from {{ref('seed_regions')}}
     )
     , transformation as (
-        select *
+        select
+            selected_customers.customer_id
+            , selected_customers.company_name
+            , selected_customers.city
+            , selected_seed_regions.region
+            , selected_customers.country
+            , selected_seed_regions.division            
         from selected_customers
         left join selected_seed_regions on selected_customers.region_id = selected_seed_regions.id
     )
     , customers as (
         select
-            row_number () over (order by customer_id) as customer_sk
-            , transformation.customer_id
-            , transformation.company_name
-            , transformation.city
-            , transformation.region
-            , transformation.country
-            , transformation.division
+            row_number() over (order by customer_id) as customer_sk
+            , *
         from transformation
     )
 select *
